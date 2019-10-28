404c26c41652f715e85461b04fcb552fc509a072
Hamza Chorfi <developerw3@gmail.com>
2019-10-28 10:12:09 -0400

    Simple react.js App - Tangerine Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Login from './components/Login';
+import Home from  './components/Home';
+import Author from  './components/Author';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export interface IProps {}
+
+export interface IState {}
+
+class App extends React.Component<IProps, IState> {
+
+  public componentDidMount() {
+    
+    this.setLogin();
+  }
+
+  public setLogin() {
+
+    const isLogin = localStorage.getItem('isLogin');
+
+    const pathname = window.location.pathname;
+
+    if((isLogin === 'false' && pathname !== '/login' && pathname !== '/author') || (isLogin === 'false' && pathname === '/author')) {
+        window.location.pathname = '/login';
+    } else if(isLogin === 'true' && pathname !== '/home' && pathname !== '/author') {
+      window.location.pathname = '/home';
+    }
+  }
+
+
+  public render() {
+      return (
+        <Router>
+          <Suspense fallback={<div></div>}>
+            <Switch>
+              <Route exact path='/' key='0'><Redirect to="/login" /></Route>
+              <Route exact path='/login' component={Login} key='1' />
+              <Route exact path='/home' component={Home} key='2' />
+              <Route exact path='/author' component={Author} key='3' />
+            </Switch>
+          </Suspense>
+        </Router>
+      );
+  }
 }
 
 export default App;
